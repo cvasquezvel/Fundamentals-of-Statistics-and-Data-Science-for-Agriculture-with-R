30bb6c9945baa8f1bdc2ce734dc41b56b6e9f5bc
Christian Richard Alberto Vásquez Velasco <91028869+cvasquezvel@users.noreply.github.com>
2024-09-16 00:34:22 -0500

    Add files via upload

---
+++ Sesion02.R
@@ -0,0 +1,35 @@
+1 / 200 * 30
+
+(59 + 73 + 2) / 2
+
+sin(pi/2)
+
+x <-  3 * 4
+
+primes <- c(2, 3, 5, 7, 11, 13)
+
+Christian <- c(primes, x)
+
+multiples <- primes * 2
+multiples
+
+?c()
+
+this_is_a_really_long_name <- 2.5
+this_is_a_really_long_name
+
+value_2023 <- 2.5
+
+r2 <- 2.3
+r2 <- 2^3
+
+Christia
+christian
+
+seq(1, 10)
+
+seq(1,10,2)
+
+x <- "hola mundo"
+x
+
